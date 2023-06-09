fb8e8d4484511b11e2b0fcfd622d02187d7f3098
pace-pdro <korwapedro@gmail.com>
2023-06-09 18:50:29 +0700

    add compose fun

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -47,4 +47,16 @@ dependencies {
     implementation("androidx.compose.foundation:foundation:1.4.3")
     implementation("androidx.compose.material:material:1.4.3")
     implementation("androidx.activity:activity-compose:1.7.1")
+
+// Ex library
+    //    Constraint layout
+    implementation ("androidx.constraintlayout:constraintlayout-compose:1.0.1")
+
+    // Kotlin
+    implementation("androidx.navigation:navigation-fragment-ktx:2.5.1")
+    implementation("androidx.navigation:navigation-ui-ktx:2.5.1")
+
+    // Jetpack Compose Integration
+    implementation("androidx.navigation:navigation-compose:2.5.1")
+
 }
--- androidApp/src/main/java/com/pacepdro/kastauibadah/android/layout/HomeScreen.kt
+++ androidApp/src/main/java/com/pacepdro/kastauibadah/android/layout/HomeScreen.kt
@@ -1,16 +1,42 @@
 package com.pacepdro.kastauibadah.android.layout
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.BottomAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.constraintlayout.compose.ConstraintLayout
 
 @Composable
 fun home(){
 
+    ConstraintLayout(modifier = Modifier
+        .fillMaxSize()
+        .background(Color.White)) {
+        val image = createRef()
+        val text = createRef()
+        val button = createRef()
+        val box = createRef()
+        val card = createRef()
+    }
+
+
+}
+
+
+@Composable
+fun BottomBar(){
+        BottomAppBar() {
+            
+        }
 }
 
 
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun homePreciew(){
-
+        home()
 }
